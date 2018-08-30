696a9995a7a6542b9d06970f6e10518350c7d7e7
ashad <ashadnasim72@gmail.com>
2018-08-30 11:42:08 +0530

    Initial commit

---
+++ app/src/main/java/byashad/qoutespicture/picture/quotes/picturequotes/Splash.java
@@ -0,0 +1,27 @@
+package byashad.qoutespicture.picture.quotes.picturequotes;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler h=new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(getApplicationContext(),quotes.class);
+                startActivity(i);
+                finish();
+
+            }
+        },1000);
+        //todo changet t
+        // o 2000
+    }
+}
