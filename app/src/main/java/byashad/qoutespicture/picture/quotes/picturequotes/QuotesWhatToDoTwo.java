979931702158ddb217cd5d47874a8f143822eb90
ashad <ashadnasim72@gmail.com>
2018-09-01 00:30:37 +0530

    verywell

---
+++ app/src/main/java/byashad/qoutespicture/picture/quotes/picturequotes/QuotesWhatToDoTwo.java
@@ -0,0 +1,13 @@
+package byashad.qoutespicture.picture.quotes.picturequotes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuotesWhatToDoTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quotes_what_to_do_two);
+    }
+}
